975ce9b072ec170d6c213cdaf798cf998a55f8c1
vanna2020 <vannasingh1999@gmail.com>
2021-09-08 07:33:54 -0700

    single player snake and ladder

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -2,7 +2,11 @@ public class SnakeAndLadder{
 
     public static void main(String[] args) {
 
-        System.out.println("welcome to snake and ladder game");
-        int position = 0;
+                int playerposition = 0;
+                System.out.println("The start position of player is : " + playerposition);
+
+                int diceroll = (int) (Math.random() * 6) + 1;
+                playerposition += diceroll;
+                System.out.println("After the roll dice....The position of player is : " + playerposition);
     }
 }
